0b0ea7e8198f6e17e83a8e6bb5c0d94efe6a0832
Pan <panb@tbkcreative.com>
2020-03-30 15:43:24 -0400

    update fetch to get demographics

--- scrapy/covid_ontario/spiders/ontario.py
+++ scrapy/covid_ontario/spiders/ontario.py
@@ -20,12 +20,16 @@ class OntarioSpider(scrapy.Spider):
             'Deceased': 'deceased',
             'Investigation': 'pending',
             'Total': 'total',
+            'Male': 'male',
+            'Female': 'female',
+            '19': 'youth',
+            '20-64': 'adult',
+            '65': 'senior',
         }
         # status_table = response.css('.field-type-text-with-summary table')[0]
         # self.logger.warning('Table HTML %s', response)
-        open_in_browser(response)
-        date_timestamp = datetime.datetime.now().strftime("%B %d, %Y")
-        status_item['date'] = date_timestamp
+        # open_in_browser(response)
+        daily_data = {}
         for row in response.xpath('//table[1]/tbody/tr'):
             name = row.xpath(
                 'td[1]/descendant-or-self::*/text()').get().strip()
@@ -37,5 +41,20 @@ class OntarioSpider(scrapy.Spider):
                     # self.logger.warning('Table Name %s', name)
                     # self.logger.warning('Label %s', label)
                     # self.logger.warning('Value %s', value)
-                    status_item[key] = value
+                    daily_data[key] = value
+
+        date_timestamp = datetime.datetime.now().strftime("%B %d, %Y")
+        status_item['date'] = date_timestamp
+        status_item['confirmed'] = {
+            'total': daily_data['confirmed'],
+            'male': daily_data['male'],
+            'female': daily_data['female'],
+            'youth': daily_data['youth'],
+            'adult': daily_data['adult'],
+            'senior': daily_data['senior'],
+        }
+        status_item['deceased'] = daily_data['deceased']
+        status_item['pending'] = daily_data['pending']
+        status_item['resolved'] = daily_data['resolved']
+        status_item['total'] = daily_data['total']
         yield status_item
