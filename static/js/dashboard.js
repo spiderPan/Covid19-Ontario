1773b42f82a5683cf22d9eda9072314abb4fdd16
Pan <panb@tbkcreative.com>
2020-03-30 16:19:17 -0400

    add chart point

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,41 +1,50 @@
-/* globals Chart:false, feather:false */
-
-(function() {
-    'use strict'
-
+jQuery(function($) {
     feather.replace()
 
     // Graphs
-    var ctx = document.getElementById('myChart')
-        // eslint-disable-next-line no-unused-vars
-    var myChart = new Chart(ctx, {
+    var ctx = $('#myChart'),
+        ontario_data = ctx.data('status'),
+        dates = [],
+        confirmed_cases = [],
+        resolved_cases = [],
+        deceased_cases = [];
+
+    console.table(ontario_data);
+    ontario_data.forEach(function(daily) {
+        dates.push(daily['date']);
+        confirmed_cases.push(daily['confirmed']['total']);
+        resolved_cases.push(daily['resolved']);
+        deceased_cases.push(daily['deceased']);
+    });
+    // eslint-disable-next-line no-unused-vars
+    var myChart = new Chart(ctx.get(), {
         type: 'line',
         data: {
-            labels: [
-                'Sunday',
-                'Monday',
-                'Tuesday',
-                'Wednesday',
-                'Thursday',
-                'Friday',
-                'Saturday'
-            ],
+            labels: dates,
             datasets: [{
-                data: [
-                    15339,
-                    21345,
-                    18483,
-                    24003,
-                    23489,
-                    24092,
-                    12034
-                ],
-                lineTension: 0,
-                backgroundColor: 'transparent',
-                borderColor: '#007bff',
-                borderWidth: 4,
-                pointBackgroundColor: '#007bff'
-            }]
+                    data: confirmed_cases,
+                    lineTension: 0,
+                    backgroundColor: 'transparent',
+                    borderColor: '#dc3545',
+                    borderWidth: 4,
+                    pointBackgroundColor: '#dc3545'
+                }, {
+                    data: resolved_cases,
+                    lineTension: 0,
+                    backgroundColor: 'transparent',
+                    borderColor: '#28a745',
+                    borderWidth: 4,
+                    pointBackgroundColor: '#28a745'
+                },
+                {
+                    data: deceased_cases,
+                    lineTension: 0,
+                    backgroundColor: 'transparent',
+                    borderColor: '#343a40',
+                    borderWidth: 4,
+                    pointBackgroundColor: '#343a40'
+                }
+            ]
         },
         options: {
             scales: {
@@ -49,5 +58,5 @@
                 display: false
             }
         }
-    })
-}())
+    });
+});
