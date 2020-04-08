ea3a36c4dfe42f9fb3c8d5e60b907b82cdaaa137
Pan <panb@tbkcreative.com>
2020-04-08 14:49:35 -0400

    display the data from mongo to table

--- app.py
+++ app.py
@@ -6,13 +6,34 @@ from datetime import datetime
 import pytz
 
 app = Flask(__name__)
-app.config['MONGO_URI'] = 'mongodb://admin:123@mongo:27017/movie_cms'
+app.config['MONGO_URI'] = 'mongodb://admin:123@mongo:27017/covid_ontario'
 mongo = PyMongo(app)
+
+
+@app.template_filter('strftime')
+def _jinja2_filter_datetime(date, fmt=None):
+    date = datetime.fromtimestamp(date)
+    native = date.replace(tzinfo=None)
+    format = '%Y-%m-%d'
+    return native.strftime(format)
 
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    status = mongo.db.status
+    status_data = []
+    for s in status.find().sort("date"):
+        status_data.append({
+            'date': _jinja2_filter_datetime(int(s.get('date'))),
+            'deceased': s.get('deceased', 0),
+            'confirmed': s.get('confirmed', 0),
+            'resolved': s.get('resolved', 0),
+            'pending': s.get('pending', 0),
+            'total': s.get('total', 0)
+        })
+
+    return render_template('home.html', ontario_data=status_data)
+
 
 @app.route('/bar-chart')
 def bar_chart():
